8e903921586c1739487b2060d5b8aaee242596ec
baguchan <shurf012@gmail.com>
2022-04-05 13:38:42 +0900

    update to 1.18.2

--- src/main/java/baguchan/endenviron/world/gen/ModConfiguredFeatures.java
+++ src/main/java/baguchan/endenviron/world/gen/ModConfiguredFeatures.java
@@ -2,7 +2,9 @@ package baguchan.endenviron.world.gen;
 
 import baguchan.endenviron.EndEnviron;
 import baguchan.endenviron.registry.ModBlocks;
+import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
@@ -10,10 +12,10 @@ import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConf
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
 public class ModConfiguredFeatures {
-	public static final ConfiguredFeature<?, ?> PATCH_END_GRASS = FeatureUtils.register(prefix("patch_end_grass"), Feature.RANDOM_PATCH.configured(grassPatch(BlockStateProvider.simple(ModBlocks.END_GRASS.get()), 32)));
+	public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_END_GRASS = FeatureUtils.register(prefix("patch_end_grass"), Feature.RANDOM_PATCH, grassPatch(BlockStateProvider.simple(ModBlocks.END_GRASS.get()), 32));
 
 	private static RandomPatchConfiguration grassPatch(BlockStateProvider p_195203_, int p_195204_) {
-		return FeatureUtils.simpleRandomPatchConfiguration(p_195204_, Feature.SIMPLE_BLOCK.configured(new SimpleBlockConfiguration(p_195203_)).onlyWhenEmpty());
+		return FeatureUtils.simpleRandomPatchConfiguration(p_195204_, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(p_195203_)));
 	}
 
 	public static String prefix(String name) {
--- src/main/java/baguchan/endenviron/world/gen/ModPlacements.java
+++ src/main/java/baguchan/endenviron/world/gen/ModPlacements.java
@@ -1,13 +1,14 @@
 package baguchan.endenviron.world.gen;
 
 import baguchan.endenviron.EndEnviron;
+import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.CountOnEveryLayerPlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class ModPlacements {
-	public static final PlacedFeature PATCH_END_GRASS = PlacementUtils.register(prefix("patch_end_grass"), ModConfiguredFeatures.PATCH_END_GRASS.placed(CountOnEveryLayerPlacement.of(6), BiomeFilter.biome()));
+	public static final Holder<PlacedFeature> PATCH_END_GRASS = PlacementUtils.register(prefix("patch_end_grass"), ModConfiguredFeatures.PATCH_END_GRASS, CountOnEveryLayerPlacement.of(6), BiomeFilter.biome());
 
 
 	public static String prefix(String name) {
