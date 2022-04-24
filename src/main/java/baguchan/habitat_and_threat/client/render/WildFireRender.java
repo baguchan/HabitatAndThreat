fff31eb9abce54e04433b390b1b5a0f269660c4c
baguchan <shurf012@gmail.com>
2022-04-24 22:25:19 +0900

    more scale

--- src/main/java/baguchan/habitat_and_threat/client/render/WildFireRender.java
+++ src/main/java/baguchan/habitat_and_threat/client/render/WildFireRender.java
@@ -4,6 +4,7 @@ import baguchan.habitat_and_threat.HabitatAndThreat;
 import baguchan.habitat_and_threat.client.ModModelLayers;
 import baguchan.habitat_and_threat.client.model.WildFireModel;
 import baguchan.habitat_and_threat.entity.WildFire;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
@@ -17,6 +18,12 @@ public class WildFireRender extends MobRenderer<WildFire, EntityModel<WildFire>>
 
 	public WildFireRender(EntityRendererProvider.Context p_173952_) {
 		super(p_173952_, new WildFireModel<>(p_173952_.bakeLayer(ModModelLayers.WILDFIRE)), 0.35F);
+	}
+
+	@Override
+	protected void scale(WildFire p_115314_, PoseStack p_115315_, float p_115316_) {
+		p_115315_.scale(1.1F, 1.1F, 1.1F);
+		super.scale(p_115314_, p_115315_, p_115316_);
 	}
 
 	@Override
